172cb9a724053dc396e8ab0c8eb1bcc3d883798b
lucas <lucas1000001@gmail.com>
2013-07-29 14:07:48 +0100

    Added chapter 4 execises

---
+++ src/main/scala/exercise4/Solutions.scala
@@ -0,0 +1,68 @@
+package exercise4
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lucas
+ * Date: 7/29/13
+ * Time: 1:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Solutions extends FlatSpec with ShouldMatchers {
+
+  "(1) Setting up a map of gizmos -> prices and producing a second map with a 10% discount" should "look like" in {
+    fail()
+  }
+
+  "(2) Reading words from a file, and using a mutable map to count how often the words appear" should "look like" in {
+    // val in = new java.util.Scanner(new java.io.File("myfile.txt")
+    // or look at chapter 9 for a Scalaesque way
+    // at the end print all words and their counts
+    fail()
+  }
+
+  "(3) Repeating (2) with an immutable map" should "look like" in {
+    fail()
+  }
+
+  "(4) Repeating (2) with a sorted map" should "print in sorted order" in {
+    fail()
+  }
+
+  "(5) Repeating (2) with java.util.TreeMap that is adopted to the Scala API" should "look like" in {
+    fail()
+  }
+
+  "(6) A linked hash  map that maps 'Monday' to java.util.Calender.MONDAY" should "maintain insertion order" in {
+    fail()
+  }
+
+  "(7) A formatted table of all Java properties" should  """ look like :
+
+                                                            java.runtime.name | Java(TM) SE Runtime Environment
+                                                            java.vm.version   | 17.0-b16
+                                                            java.vm.vendor    | Sun Microsystems Inc.
+                                                            ...
+
+                                                         """ in {
+    fail()
+  }
+
+  "(8) minmax(values:Array[Int])" should "return a pair containing the smallest and largest in the array" in {
+    fail()
+  }
+
+  "(9) lteqgt(values:Array[Int], v:Int)" should "return a triple containing the counts of values less than, equal to and greater than v" in {
+    fail()
+  }
+
+  "(10) Zipping two strings, such as 'Hello'.zip('World')" should "look like" in {
+    // Come up with a plausible use case
+    fail()
+  }
+
+
+
+}
